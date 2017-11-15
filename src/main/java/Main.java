958345a67ffde77fe5e8eea3389317a28e1f470e
Volodymyr Zabulskyy <v.zabulskyy@gmail.com>
2017-11-16 00:14:48 +0200

    Test implementations removed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            GameManager.fight(CharacterFactory.createCharacter(), CharacterFactory.createCharacter());
-        }
+        GameManager.fight(CharacterFactory.createCharacter(), CharacterFactory.createCharacter());
     }
 }
